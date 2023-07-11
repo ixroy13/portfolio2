c78f8cce410c72c9d090c54c41a901bc558ec8e4
Olek Pal <palaleksander777@gmail.com>
2023-07-11 22:00:16 +0200

    Add folder with styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./components/style.css"
+import "./styles/style.css"
 import Navbar from "./components/Navbar"
 import Background from "./components/Background";
 import Hero from "./components/Hero";
