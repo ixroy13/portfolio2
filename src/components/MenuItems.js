03bba300d461c695284d0ad39686646e9daa2383
Olek Pal <palaleksander777@gmail.com>
2023-07-12 17:53:14 +0200

    Add new files

---
+++ src/components/MenuItems.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function MenuList() {
+    return (
+        <ul>
+            <li>
+                <a href="#">Blockchain</a>
+            </li>
+            <li>
+                <a href="#">Tic Tac Toe</a>
+            </li>
+            <li>
+                <a href="#">Rock Paper Scissors</a>
+            </li>
+            <li>
+                <a href="#">Ecomerce</a>
+            </li>
+        </ul>
+    )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/SkillsSection.css"
+
+export default function SkillsSection() {
+    return (
+        <section className="skills">
+            <div className="title">
+                <h1>Skills</h1>
+            </div>
+            <div className="skills-level">
+
+            </div>
+        </section>
+    )
+}
