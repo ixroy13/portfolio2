aa917eece7af0a8bf71563a86d808e39e4b41b75
Olek Pal <palaleksander777@gmail.com>
2024-01-13 18:45:54 +0100

    Bug fix on Apple OS

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,7 @@ export default function Project(props) {
     const projectDate = new Date(props.date)
     const today = new Date()
     const timeDifference = today.getTime() - projectDate.getTime();
-    const timeDifferenceInWeeks = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 7))
+    const timeDifferenceInWeeks = (Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 7))).toString()
 
     return (
       <a href={props.link}>
@@ -13,7 +13,7 @@ export default function Project(props) {
           <div className="information">
             <h1>{props.title}</h1>
             <p>{props.tech}</p>
-            <p>Made {timeDifferenceInWeeks.toString()} weeks ago</p>
+            <p>Made {timeDifferenceInWeeks} weeks ago</p>
           </div>
           <div>
             <p>{props.describe}</p>
